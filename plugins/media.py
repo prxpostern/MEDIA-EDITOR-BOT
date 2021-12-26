a9def84f75f250d14fbc38503e1b599635312e9f
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-12-26 11:54:00 +0330

    Update media.py

--- plugins/media.py
+++ plugins/media.py
@@ -8,71 +8,74 @@ PACK = filters.animation | filters.document| filters.video|filters.audio |filter
 
 @Client.on_message(PACK  & filters.private)
 async def media(client, message):
-    
-     if message.photo:
+    if message.photo:
         file_id = message.photo.file_id
         mid = InputMediaPhoto(file_id, caption=message.caption and message.caption.html)
 
-     elif message.document:
+    elif message.document:
         file_id = message.document.file_id
         mid = InputMediaDocument(file_id, caption=message.caption and message.caption.html)
 
-     elif message.video:
+    elif message.video:
         file_id = message.video.file_id
         mid = InputMediaVideo(file_id, caption=message.caption and message.caption.html)
 
-     elif message.animation:
+    elif message.animation:
         file_id = message.animation.file_id
         mid = InputMediaAnimation(file_id, caption=message.caption and message.caption.html)
 
-     elif message.audio:
+    elif message.audio:
         file_id  = message.audio.file_id
         mid = InputMediaAudio(file_id, caption=message.caption and message.caption.html)
-     else:
+    else:
         print('no way')
 
-     try:
-         a = await client.ask(message.chat.id,'Now send me the link of the message of the channnel that you need to edit',
+    try:
+        a = await client.ask(message.chat.id,'Now send me the link of the message of the channnel that you need to edit',
                     filters=filters.text, timeout=30)
-         b = await client.ask(message.chat.id,'Now send me Duration:',
+        b = await client.ask(message.chat.id,'Now send me Duration:',
                     filters=filters.text, timeout=30)
-     except TimeoutError:
-         await message.reply_text(
-           "```Session Timed Out.Resend the file to Start again```",
-           parse_mode="md",
-           quote=True
-         )
-           return
-     link = a.text
-     if  message.video:
-         duration = int(b.text)
+    except TimeoutError:
+        await message.reply_text(
+            "```Session Timed Out.Resend the file to Start again```",
+            parse_mode="md",
+            quote=True
+        )
+        return
+    
+    link = a.text
+    if  message.video:
+        duration = int(b.text)
+        file_id = message.video.file_id
+        mid = InputMediaVideo(file_id, caption=message.caption and message.caption.htmlو duration=duration)
+            
          
-     a = "-100"
-     try:
+    a = "-100"
+    try:
          id = link.split('/')[4]
          msg_id = link.split('/')[5]
          cd = a + str(id)
          chid = int(cd)
 
-     except:
+    except:
          chid = link.split('/')[3]
          msg_id = link.split('/')[4]    
-     try:
+    try:
          is_admin=await client.get_chat_member(chat_id=chid, user_id=message.from_user.id)
-     except UserNotParticipant:
+    except UserNotParticipant:
          await message.reply("It seems you are not a member of this channel and hence you can't do this action.")
          return
-     if not is_admin.can_edit_messages:
+    if not is_admin.can_edit_messages:
          await message.reply("You are not permited to do this, since you do not have the right to edit posts in this channel.")
          return
             
-     try:
+    try:
         await client.edit_message_media(
           chat_id = chid,
           message_id = int(msg_id),
           media = mid
         )
-     except Exception as e:
+    except Exception as e:
           await message.reply_text(e)
           return
-     await message.reply_text('**successfully Edited the media**')
+    await message.reply_text('**successfully Edited the media**')
